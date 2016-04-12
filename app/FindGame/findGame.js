1409603695d124cb4ba0ae142648bb624e3d4f12
Donia <donia.mekni@esprit.tn>
2016-04-12 18:29:05 +0200

    hhh

--- app/FindGame/findGame.js
+++ app/FindGame/findGame.js
@@ -1,37 +1,62 @@
 'use strict';
 
-angular.module('myApp.FindGame', ['ngCordova'])
+angular.module('myApp.FindGame', ['ngCordova','ngResource'])
  
-.controller('FindGameCtrl', function($scope,$cordovaGeolocation) {
-        
-        
+.controller('FindGameCtrl', function($scope, $cordovaGeolocation, GamesFactory, GroundsFactory) {
+    var
+    listGrounds = GroundsFactory.query();
+console.log(listGrounds);
+        var
+    listGames = GamesFactory.query();
+    console.log(listGames);
+
+
+    //for(var i=0; i<listGames.length; i++){
+    //    for(var j=0; j<listGrounds.length; j++) {
+    //
+    //       if ( listGames[i].d.groundID == listGrounds[j].d._id){
+    //           listGames[i].location = listGrounds[j].name;
+    //           console.log(listGames[i].groundID);
+    //       }
+    //    }
+    //}
+
+    $scope.games = listGames;
+    console.log($scope.games);
+    console.log(listGames);
      /////////////////////MAP//////////////////////
    // $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-    
-    
     var options = {timeout: 10000, enableHighAccuracy: true};
- 
     $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-    var mapOptions = {
+        var mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-       
-        
-        
- 
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
   }, function(error){
     console.log("Could not get location");
   });
-    
-    
     //////////////////////////////////////////////////
 
+
 })
+
+    .factory('GamesFactory', function($resource){
+        //Resource
+        return $resource('http://localhost:3000/events');
+
+    })
+    .factory('GameDetailsFactory', function($resource){
+        //Resource
+        return $resource('http://localhost:3000/events/:id');
+
+    })
+
+    .factory('GroundsFactory', function($resource){
+        //Resource
+        return $resource('http://localhost:3000/grounds/grounds');
+
+    })
 ;
