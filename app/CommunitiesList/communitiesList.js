ba8cb4a5a958abb773c24f18a92bb21cac0afcac
HaifaKad <kadrihaifa@gmail.com>
2016-05-02 01:33:36 +0200

    .

--- app/CommunitiesList/communitiesList.js
+++ app/CommunitiesList/communitiesList.js
@@ -1,8 +1,20 @@
 'use strict';
 
-angular.module('myApp.communitiesList', ['ngRoute'])
+angular.module('myApp.communitiesList', ['ngRoute','ngResource'])
 
+.factory('CommunityAndTeams',function($resource) {
+  return $resource('http://localhost:4000/community/all');
 
-.controller('communitiesListCtrl', [function() {
+})
+.controller('communitiesListCtrl', function($scope, CommunityAndTeams,$http,$route,$routeParams,$location) {
+         $scope.communities = CommunityAndTeams.query();
+    
 
-}]);
+    
+    $scope.go = function (id_team) {
+  $location.path('/invite/'+$routeParams.param+"/"+id_team);
+};
+    
+    $scope.communityid = $routeParams.param; 
+      
+});
