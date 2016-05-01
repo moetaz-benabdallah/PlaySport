f01b1326c952a9e3554766285fe28f25a29e9c3b
HaifaKad <kadrihaifa@gmail.com>
2016-05-02 01:34:31 +0200

    .

--- app/Invitations/invitations.js
+++ app/Invitations/invitations.js
@@ -1,8 +1,47 @@
 'use strict';
 
-angular.module('myApp.invitations', ['ngRoute'])
+angular.module('myApp.invitations', ['ngRoute','ngResource'])
+
+.factory('Invitations',function($resource) {
+  return $resource('http://localhost:4000/invitation/:id',{id: "@id"});
 
 
-.controller('invitationsCtrl', [function() {
+})
 
-}]);
+.controller('invitationsCtrl', function($scope, Invitations,$http,$route,$routeParams) {
+   
+   $http.get("http://localhost:4000/invitation/"+$routeParams.param)
+   .then(function(response) {
+       
+        $scope.invitations = response.data;
+       console.log($scope.invitations);
+    });
+    
+    $scope.communityid = $routeParams.param; 
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
