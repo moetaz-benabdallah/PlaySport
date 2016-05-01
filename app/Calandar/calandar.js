31a6283b62f9d61b0d2765e79cce233de47d419e
HaifaKad <kadrihaifa@gmail.com>
2016-05-02 01:31:33 +0200

    .

--- app/Calandar/calandar.js
+++ app/Calandar/calandar.js
@@ -1,8 +1,34 @@
 'use strict';
 
-angular.module('myApp.calandar', ['ngRoute'])
+angular.module('myApp.calandar', ['ngRoute','ngResource'])
+
+.factory('Calandar',function($resource) {
+  return $resource('http://localhost:4000/game');
+})
 
 
-.controller('calandarCtrl', [function() {
+.controller('calandarCtrl', function($scope, Calandar,$http,$route,$routeParams) {
+       
+    $scope.communityid = $routeParams.param; 
+     $scope.gameData = {};
+    $scope.newgame = function() {
+         $scope.gameData._teams=$routeParams.param ;
+           
+             console.log( $scope.gameData);
+             
+        var game = new Calandar($scope.gameData);
+        game.$save();
+        $route.reload();  
+        
+      
+  }
+    
+       $http.get("http://localhost:4000/game/"+$routeParams.param)
+   .then(function(response) {
+       
+        $scope.games = response.data;
+     
+    });
+    
 
-}]);
+});
