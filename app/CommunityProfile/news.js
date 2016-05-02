11c03f5bd9f85aad6e2709ce938f4edda0e9d9df
HaifaKad <kadrihaifa@gmail.com>
2016-05-02 11:53:37 +0200

    news js

---
+++ app/CommunityProfile/news.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('myApp.news', ['ngRoute','ngResource'])
+
+.factory('News',function($resource) {
+  return $resource('http://localhost:4000/tweeter/news',{id:'@id'},
+ { remove: { method: "DELETE", url: "http://localhost:4000/tweeter/deletenews/:id" },
+   update: { method: 'PUT',url : "http://localhost:4000/tweeter/updatenews/:id" }
+ });
+
+
+})
+
+.controller('newsCtrl', function($scope, News,$http,$route,$routeParams) {
+    $scope.newsData = {};
+    $scope.newNews = function() {
+      $scope.newsData.communityName=$routeParams.param ;
+    var news = new News($scope.newsData);
+    news.$save();
+    $route.reload();  
+  }  
+    
+      $http.get("http://localhost:4000/tweeter/news/community/"+$routeParams.param)
+     .then(function(response) {
+        $scope.news = response.data;
+    });
+    
+    $scope.deleteNews=function(id) {
+       News.remove({id: id})
+       $route.reload();     
+    }
+    
+    $scope.updateNews = function(id) { 
+        News.update({id: id});
+    }
+    
+    $scope.communityid = $routeParams.param; 
+    
+  });
